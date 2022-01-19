913530da6eb59e2445faa244c783bb8702be90b8
明奋 <xuhong.txh@alibaba-inc.com>
2022-01-19 15:37:07 +0800

    add the code

---
+++ app/src/main/jni/Application.mk
@@ -0,0 +1,4 @@
+APP_ABI := all
+NDK_TOOLCHAIN_VERSION = 4.7
+APP_PLATFORM    := android-9
+APP_CPPFLAGS    := -fexceptions -frtti
---
+++ app/src/main/obj/local/x86/objs/native-audio-jni/native-audio-jni.o.d
@@ -0,0 +1,8 @@
+/Users/darren/work/code/androidProject/native-audio/app/src/main/obj/local/x86/objs/native-audio-jni/native-audio-jni.o: \
+  /Users/darren/work/code/androidProject/native-audio/app/src/main/jni/native-audio-jni.c \
+  /Users/darren/work/code/androidProject/native-audio/app/src/main/jni/hello_clip.h \
+  /Users/darren/work/code/androidProject/native-audio/app/src/main/jni/android_clip.h
+
+/Users/darren/work/code/androidProject/native-audio/app/src/main/jni/hello_clip.h:
+
+/Users/darren/work/code/androidProject/native-audio/app/src/main/jni/android_clip.h:
